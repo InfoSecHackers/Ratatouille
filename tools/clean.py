d63da3c43cfd3ab3c1f9423c46f23221cbe6d93f
01Odin <abhishekmani0007@gmail.com>
2019-08-30 10:07:50 -0700

    test2

--- tools/clean.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def clean():
-	
-
-	os.system("sudo rsync -av pcap_downloads/ old_pacp/")
-	os.system("sudo rm -R pcap_downloads/*")
-	print "All files moved to old_pacp download folder is clean now"
